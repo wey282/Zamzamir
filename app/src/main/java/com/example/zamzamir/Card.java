1d8498eb1356858806a2ffd76c500f2b021b4ea0
wey282 <lev.daniel.sar@gmail.com>
2025-01-29 22:23:32 +0200

    Added card selection

--- app/src/main/java/com/example/zamzamir/Card.java
+++ app/src/main/java/com/example/zamzamir/Card.java
@@ -9,30 +9,33 @@ import java.util.List;
 
 public class Card extends Point {
 	public static final List<Card> deck = new ArrayList<>();
+	private static final List<Card> cards = new ArrayList<>();
 
-	public static final int WIDTH = 189, HEIGHT = 264;
+	public static final int WIDTH = 156, HEIGHT = 220;
 
 	public static Bitmap back;
 
+	private final Bitmap fullSprite;
 	private final Bitmap sprite;
-	private final Bitmap smallBitmap;
 	private boolean revealed;
 
 	private int attackValue;
 	private int defenceValue;
 
+	private int owner = -1;
+
 	public Card(Bitmap sprite, int attackValue, int defenceValue) {
-		this.sprite = sprite;
-		this.smallBitmap = Bitmap.createScaledBitmap(sprite, WIDTH, HEIGHT, false);
+		this.fullSprite = sprite;
+		this.sprite = Bitmap.createScaledBitmap(sprite, WIDTH, HEIGHT, false);
 		this.attackValue = attackValue;
 		this.defenceValue = defenceValue;
-		x = (int)(Math.random()*1000);
 		deck.add(this);
+		cards.add(this);
 	}
 
 	/** Draws the card on the screen.*/
 	public void draw(Canvas canvas) {
-		canvas.drawBitmap(revealed ? smallBitmap : back, x, y, null);
+		canvas.drawBitmap(revealed ? sprite : back, x, y, null);
 	}
 
 	public void setRevealed(boolean revealed) {
@@ -45,5 +48,37 @@ public class Card extends Point {
 
 	public int getDefenceValue() {
 		return defenceValue;
+	}
+
+	public Bitmap getFullSprite() {
+		return fullSprite;
+	}
+
+	public void setOwner(int player) {
+		owner = player;
+	}
+
+
+	public static Card checkTouchForAllCards(float x, float y) {
+		for (Card card: cards) {
+			if (x > card.x && card.x + WIDTH  > x
+		  	 && y > card.y && card.y + HEIGHT > y)
+				return card;
+		}
+		return null;
+	}
+
+	public static Card checkTouchForKnownCards(float x, float y, int player) {
+		for (Card card: cards) {
+			if (x > card.x && card.x + WIDTH  > x
+					&& y > card.y && card.y + HEIGHT > y
+					&& (card.revealed || player == card.owner))
+				return card;
+		}
+		return null;
+	}
+
+	public int getOwner() {
+		return owner;
 	}
 }
--- app/src/main/java/com/example/zamzamir/GameView.java
+++ app/src/main/java/com/example/zamzamir/GameView.java
@@ -1,13 +1,19 @@
 package com.example.zamzamir;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +21,17 @@ import java.util.List;
 public class GameView extends View {
 	private List<Card> discardPile;
 	private List<List<Card>> players;
-	private int currentPlayer;
+
+	private int currentPlayer = 0;
 	private int player;
+
+	private Card selectedCard;
+	private Card confirmedCard;
+	private Card targetCard;
+
+	private Paint highlightPaint;
+	private Paint confirmedPaint;
+	private Paint targetedPaint;
 
 	public GameView(Context context) {
 		super(context);
@@ -35,6 +50,43 @@ public class GameView extends View {
 
 	private void init() {
 		discardPile = new ArrayList<>();
+
+		setOnTouchListener(this::onTouch);
+
+		highlightPaint = new Paint();
+		highlightPaint.setStyle(Paint.Style.STROKE);
+		highlightPaint.setStrokeJoin(Paint.Join.ROUND);
+		highlightPaint.setStrokeWidth(40);
+		highlightPaint.setColor(ContextCompat.getColor(getContext(), R.color.highlight));
+
+		confirmedPaint = new Paint(highlightPaint);
+		confirmedPaint.setStrokeWidth(50);
+		confirmedPaint.setColor(ContextCompat.getColor(getContext(), R.color.confirmed));
+
+		targetedPaint = new Paint(confirmedPaint);
+		targetedPaint.setColor(ContextCompat.getColor(getContext(), R.color.targeted));
+	}
+
+	private boolean onTouch(View view, MotionEvent event) {
+
+		if (currentPlayer == player) {
+			if (event.getAction() == MotionEvent.ACTION_DOWN) {
+				Card card = Card.checkTouchForAllCards(event.getX(), event.getY());
+				if (card != null) {
+					if (card.getOwner() == player) {
+						if (card == selectedCard)
+							confirmedCard = card;
+					} else
+						targetCard = card;
+				}
+			}
+		}
+		else {
+
+		}
+		selectedCard = Card.checkTouchForKnownCards(event.getX(), event.getY(), player);
+		invalidate();
+		return true;
 	}
 
 	public void start(int playerCount, int player)
@@ -47,15 +99,22 @@ public class GameView extends View {
 		dealCards();
 	}
 
-	/** Deals cards to all players, 2 hidden, one revealed*/
+	/** Deals cards to all players, 2 hidden, one revealed */
 	private void dealCards() {
-		for (List<Card> player: players) {
+		for (int i = 0; i < players.size(); i++) {
+			List<Card> player = players.get(i);
+			// deal 1 hidden card
 			Card card = Card.deck.remove(0);
+			card.setOwner(i);
 			player.add(card);
+			// deal 1 revealed card
 			card = Card.deck.remove(0);
 			card.setRevealed(true);
+			card.setOwner(i);
 			player.add(card);
+			// deal 1 hidden card
 			card = Card.deck.remove(0);
+			card.setOwner(i);
 			player.add(card);
 		}
 	}
@@ -69,21 +128,26 @@ public class GameView extends View {
 			card.x = cx;
 			card.y = cy;
 		}
-		Card.deck.get(0).draw(canvas);
+		if (!Card.deck.isEmpty())
+			Card.deck.get(0).draw(canvas);
+		if (selectedCard != null)
+			canvas.drawRect(selectedCard.x, selectedCard.y, selectedCard.x + Card.WIDTH, selectedCard.y + Card.HEIGHT, highlightPaint);
+		if (confirmedCard != null)
+			canvas.drawRect(confirmedCard.x, confirmedCard.y, confirmedCard.x + Card.WIDTH, confirmedCard.y + Card.HEIGHT, confirmedPaint);
+		if (targetCard != null)
+			canvas.drawRect(targetCard.x, targetCard.y, targetCard.x + Card.WIDTH, targetCard.y + Card.HEIGHT, targetedPaint);
 		for (int i = 0; i < players.size(); i++) {
 			List<Card> player = players.get(i);
 			double angle = Math.PI*2*i/players.size()+Math.PI/2;
 			for (int j = 0; j < player.size(); j++) {
 				Card card = player.get(j);
-				card.x = cx + (int)(Math.cos(angle)*Card.HEIGHT*1.2) + (int)((j - (double)player.size() /2 + 0.5) * Card.WIDTH * 1.5);
-				card.y = cy + (int)(Math.sin(angle)*Card.HEIGHT*1.2);
+				card.x = cx + (int)(Math.cos(angle)*Card.HEIGHT*1.66) + (int)((j - (double)player.size() /2 + 0.5) * Card.WIDTH * 1.5);
+				card.y = cy + (int)(Math.sin(angle)*Card.HEIGHT*1.66);
 				card.draw(canvas);
 			}
 		}
-	}
-
-	/** Kills current update thread if already running and start a thread called every 1000/TARGET_FPS milliseconds*/
-	private void startUpdate() {
-
+		if (selectedCard != null) {
+			canvas.drawBitmap(selectedCard.getFullSprite(), 0, 0, null);
+		}
 	}
 }
